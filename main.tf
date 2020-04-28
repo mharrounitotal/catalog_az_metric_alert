edf884bbbc5a1e958ba1372fff51a9cd64d189bd
kevin.gamiette <kevin.gamiette@exakis-nelite.com>
2020-04-28 21:54:57 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -40,10 +40,10 @@ module "create_ram_alert"{
     assie_rgLocation          = local.l_rg_location
     assie_rgTags              = local.l_tags
     module_action_group_id    = "${var.action_group_id}"
-    alert_code                = "ram"
+    alert_code                = "performanceCounters/memoryAvailableBytes"
     alert_index               = "${var.alert_index}"
     scope_assie_vm_id         = "${var.vm_id}"
-    alert_description         = "ram alert"
+    alert_description         = "Available memory alert"
     alert_severity            = "${var.module_severity}"
     criteria_threshold        = "${var.ram_threshold}"
 }
@@ -57,10 +57,10 @@ module "create_av_alert"{
     assie_rgLocation          = local.l_rg_location
     assie_rgTags              = local.l_tags
     module_action_group_id    = "${var.action_group_id}"
-    alert_code                = "av"
+    alert_code                = "availabilityResults/availabilityPercentage"
     alert_index               = "${var.alert_index}"
     scope_assie_vm_id         = "${var.vm_id}"
-    alert_description         = "av alert"
+    alert_description         = "Availability alert"
     alert_severity            = "${var.module_severity}"
     criteria_operator         = "${var.av_criteria_operator}"
     criteria_threshold        = "${var.av_threshold}"
@@ -93,10 +93,10 @@ module "create_srt_alert"{
     assie_rgLocation          = local.l_rg_location
     assie_rgTags              = local.l_tags
     module_action_group_id    = "${var.action_group_id}"
-    alert_code                = "srt"
+    alert_code                = "requests/duration"
     alert_index               = "${var.alert_index}"
     scope_assie_vm_id         = "${var.vm_id}"
-    alert_description         = "srt alert"
+    alert_description         = "server response time alert"
     alert_severity            = "${var.module_severity}"
     criteria_threshold        = "${var.srt_threshold}"
 
@@ -111,10 +111,10 @@ module "create_hret_alert"{
     assie_rgLocation          = local.l_rg_location
     assie_rgTags              = local.l_tags
     module_action_group_id    = "${var.action_group_id}"
-    alert_code                = "hret"
+    alert_code                = "performanceCounters/requestExecutionTime"
     alert_index               = "${var.alert_index}"
     scope_assie_vm_id         = "${var.vm_id}"
-    alert_description         = "hret alert"
+    alert_description         = "HTTP request execution time alert"
     alert_severity            = "${var.module_severity}"
     criteria_threshold        = "${var.hret_threshold}"
 
@@ -131,7 +131,7 @@ module "create_fr_alert"{
     alert_code                = "requests/failed"
     alert_index               = "${var.alert_index}"
     scope_assie_vm_id         = "${var.vm_id}"
-    alert_description         = "fr alert"
+    alert_description         = "Failed requests alert"
     alert_severity            = "${var.module_severity}"
     criteria_aggregation      = "${var.fr_criteria_aggregation}"
     criteria_threshold        = "${var.fr_threshold}"
